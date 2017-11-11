c70733538037111c3d90a00143b70c18e5dbe4d9
tsukabiz <github@tsuka.biz>
2017-11-11 15:53:27 +0900

    Add files via upload

--- keyboards/lets_split/keymaps/tsuka/keymap.c
+++ keyboards/lets_split/keymaps/tsuka/keymap.c
@@ -58,9 +58,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------------. ,-----------------------------------------------.
- * |       |   ~   |   [   |   ]   |   %   |   &   | |  F12  |   F7  |   F8  |   F9  |       |       |
+ * |       |   ~   |   [   |   ]   |   %   |   $   | |  F12  |   F7  |   F8  |   F9  |       |       |
  * |-------+-------+-------+-------+-------+-------| |-------+-------+-------+-------+-------+-------|
- * |       |   @   |   (   |   )   |   _   |   \   | |  F11  |   F4  |   F5  |   F6  |       |       |
+ * |       |   &   |   (   |   )   |   _   |   \   | |  F11  |   F4  |   F5  |   F6  |       |       |
  * |-------+-------+-------+-------+-------+-------| |-------+-------+-------+-------+-------+-------|
  * |       |   !   |   {   |   }   |   ?   |   #   | |  F10  |   F1  |   F2  |   F3  |       |       |
  * |-------+-------+-------+-------+-------+-------| |-------+-------+-------+-------+-------+-------|
@@ -68,17 +68,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------' `-----------------------------------------------'
  */
 [_LOWER] = KEYMAP( \
-  XXXXXXX, KC_TILD, KC_LBRC, KC_RBRC, KC_PERC, KC_AMPR, KC_F12,  KC_F7,   KC_F8,   KC_F9,   XXXXXXX, XXXXXXX, \
-  XXXXXXX, KC_AT,   KC_LPRN, KC_RPRN, KC_UNDS, KC_BSLS, KC_F11,  KC_F4,   KC_F5,   KC_F6,   XXXXXXX, XXXXXXX, \
+  XXXXXXX, KC_TILD, KC_LBRC, KC_RBRC, KC_PERC, KC_DLR,  KC_F12,  KC_F7,   KC_F8,   KC_F9,   XXXXXXX, XXXXXXX, \
+  XXXXXXX, KC_AMPR, KC_LPRN, KC_RPRN, KC_UNDS, KC_BSLS, KC_F11,  KC_F4,   KC_F5,   KC_F6,   XXXXXXX, XXXXXXX, \
   XXXXXXX, KC_EXLM, KC_LCBR, KC_RCBR, KC_QUES, KC_HASH, KC_F10,  KC_F1,   KC_F2,   KC_F3,   XXXXXXX, XXXXXXX, \
   XXXXXXX, XXXXXXX, CTLHOME, CTLEND,  _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_END,  XXXXXXX, XXXXXXX  \
 ),
 
 /* Raise
  * ,-----------------------------------------------. ,-----------------------------------------------.
- * |       |   `   |       |       |       |   $   | |   *   |   7   |   8   |   9   |   =   |  Del  |
+ * |       |   `   |       |       |       |       | |   *   |   7   |   8   |   9   |   =   |  Del  |
  * |-------+-------+-------+-------+-------+-------| |-------+-------+-------+-------+-------+-------|
- * |       |       |       |       |       |   |   | |   +   |   4   |   5   |   6   |   -   | Enter |
+ * |       |   @   |       |       |       |   |   | |   +   |   4   |   5   |   6   |   -   | Enter |
  * |-------+-------+-------+-------+-------+-------| |-------+-------+-------+-------+-------+-------|
  * |       |       |       |       |       |   ^   | |   0   |   1   |   2   |   3   |   /   |       |
  * |-------+-------+-------+-------+-------+-------| |-------+-------+-------+-------+-------+-------|
@@ -86,8 +86,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------' `-----------------------------------------------'
  */
 [_RAISE] = KEYMAP( \
-  XXXXXXX, KC_GRV,  XXXXXXX, XXXXXXX, XXXXXXX, KC_DLR,  KC_ASTR, KC_7,    KC_8,    KC_9,    KC_EQL,  KC_DEL,  \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE, KC_PLUS, KC_4,    KC_5,    KC_6,    KC_MINS, _______, \
+  XXXXXXX, KC_GRV,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_ASTR, KC_7,    KC_8,    KC_9,    KC_EQL,  KC_DEL,  \
+  XXXXXXX, KC_AT,   XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE, KC_PLUS, KC_4,    KC_5,    KC_6,    KC_MINS, _______, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_CIRC, KC_0,    KC_1,    KC_2,    KC_3,    KC_SLSH, XXXXXXX, \
   XXXXXXX, XXXXXXX, KC_PGUP, KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX, _______, CTLPGUP, CTLPGDN, XXXXXXX, XXXXXXX  \
 ),
